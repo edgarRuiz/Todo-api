6b47a5c1adb310f4c4477ac317afc344c734e8e8
Edgar Ruiz <ed.ruiz1106@gmail.com>
2016-07-24 15:25:53 -0400

    Refactored server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var bodyParser = require('body-parser');
 
+var _ = require('underscore');
+
 var app = express();
 
 var PORT = process.env.PORT || 3000;
@@ -40,7 +42,14 @@ app.get('/todos/:id',function(req,res){
 
 app.post('/todos',function(req,res){
 
-	var body = req.body;
+	var body =  _.pick(req.body, 'description', 'completed');
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ){
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+
 	body.id = todoNextID++;
 	todos.push(body);
 	res.json(body);
